d622816a5b964c332d0c1bc926ca7ddc1a0c7fdc
camunozn <camunozn89@gmail.com>
2022-06-27 18:19:55 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+// https://forkify-api.herokuapp.com/v2
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '79e5fa85-e5d6-4430-88e2-f4323d09ba19';
+export const MODAL_CLOSE_SEC = 1;
